26c5dfaf9743f5d49c20607f7b1358bb19c767b7
vagrant <federico@init.uy>
2021-03-04 00:09:10 +0000

    findAdminByEmail

--- mongo/connect.js
+++ mongo/connect.js
@@ -7,35 +7,26 @@ var options = {
   useUnifiedTopology: true
 };
 
-module.exports = {
-  connect: function connect() {
-    var user = process.env.MONGO_USER
-      , pass = encodeURIComponent(process.env.MONGO_PWD)
-      , host = process.env.MONGO_HOST
-      , name = process.env.MONGO_NAME || 'scalestack-test';
+module.exports = function connect() {
+  var user = process.env.MONGO_USER
+    , pass = encodeURIComponent(process.env.MONGO_PWD)
+    , host = process.env.MONGO_HOST
+    , name = process.env.MONGO_NAME || 'crowdfunding-test';
 
-    var url = process.env.MONGO_HOST
-      ? `mongodb+srv://${user}:${pass}@${host}/${name}?retryWrites=true&w=majority`
-      : 'mongodb://localhost:27017';
+  var url = process.env.MONGO_HOST
+    ? `mongodb+srv://${user}:${pass}@${host}/${name}?retryWrites=true&w=majority`
+    : 'mongodb://localhost:27017';
 
-    return Promise.resolve()
-      .then(function () {
-        if (client) return client;
-        return MongoClient.connect(url, options);
-      })
-      .then(function (newClient) {
-        client = newClient;
-        return client.db(name);
-      })
-      .catch(function (error) {
-        console.log('MONGO CONNECTION ERROR', error);
-      });
-  },
-  close: function close() {
-    if (client) {
-      client.close();
-      client = null;
-    }
-    return Promise.resolve();
-  }
+  return Promise.resolve()
+    .then(function () {
+      if (client) return client;
+      return MongoClient.connect(url, options);
+    })
+    .then(function (newClient) {
+      client = newClient;
+      return client.db(name);
+    })
+    .catch(function (error) {
+      console.log('MONGO CONNECTION ERROR', error);
+    });
 };
