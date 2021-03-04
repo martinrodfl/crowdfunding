7dbdaba2437b277841ba54807992e9a41e76159a
vagrant <federico@init.uy>
2021-03-04 00:31:55 +0000

    saveSessionForAdmin

---
+++ save_session_for_admin.js
@@ -0,0 +1,9 @@
+const connect = require('./mongo/connect.js');
+
+module.exports = async function saveSessionForAdmin(adminId) {
+  if (!adminId) return Promise.resolve();
+  var db = await connect();
+  return db.collection('adminSessions').insertOne({
+    adminId: adminId
+  });
+};
