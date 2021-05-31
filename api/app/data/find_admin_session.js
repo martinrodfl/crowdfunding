0431aada0f03a5249500832f24c2dd5657e99565
santistep <santiasb@gmail.com>
2021-05-31 12:02:44 +0000

    adminSession promesa

--- api/app/data/find_admin_session.js
+++ api/app/data/find_admin_session.js
@@ -1,6 +1,8 @@
-module.exports = function findAdminSession(deps) {
-  return async function (payload) {
-    var adminSession = await deps.findAdminSession(payload.adminSessionId);
+module.exports = function findAdminSession(deps) {//viene del require del test y recibe dependencias
+
+  return async function (payload) { //retorna otra funcion que recibe el payload(input) LA MAGIA ESTA ACA
+
+    var adminSession = await deps.findAdminSession(payload.adminSessionId);// tiene acceso al deps y al input
     if (!adminSession) return Promise.reject({ adminSession: 'NOT_FOUND' });
     payload.adminSession = adminSession;
     return Promise.resolve(payload);
--- api/app/spec/data/find_admin_session_spec.js
+++ api/app/spec/data/find_admin_session_spec.js
@@ -1,18 +1,21 @@
 describe('findAdminSession', function () {
   var fn = require('../../data/find_admin_session.js');
 
+  
+  // DOBLE QUE SIMULA SESION EXITOSA
   function databaseFindAdminSessionDoubleSuccess(params) {
     databaseFindAdminSessionDoubleSuccess.params = params;
     databaseFindAdminSessionDoubleSuccess.result = Math.random();
-    return databaseFindAdminSessionDoubleSuccess.result;
+    return Promise.resolve(databaseFindAdminSessionDoubleSuccess.result);
   }
 
+  // DOBLE QUE SIMULA SESION NULA
   function databaseFindAdminSessionDoubleNull() {
-    return null;
+    return Promise.resolve(null);
   }
 
   it('doesnt find session', function (done) {
-    var deps = { findAdminSession: databaseFindAdminSessionDoubleNull };
+    var deps = { findAdminSession: databaseFindAdminSessionDoubleNull };//aca le pasamos el doble (dependencia)
     var payload = { adminSessionId: Math.random() };
     fn(deps)(payload).catch(function (response) {
       expect(response).toEqual({ adminSession: 'NOT_FOUND' });
@@ -21,11 +24,11 @@ describe('findAdminSession', function () {
   });
 
   it('find session', function (done) {
-    var deps = { findAdminSession: databaseFindAdminSessionDoubleSuccess };
+    var deps = { findAdminSession: databaseFindAdminSessionDoubleSuccess };//aca le pasamos el doble (dependencia)
     var input = { adminSessionId: Math.random() };
     fn(deps)(input).then(function (output) {
       expect(output).toBe(input);
-      expect(databaseFindAdminSessionDoubleSuccess.params).toBe(input.adminSessionId);
+      expect(databaseFindAdminSessionDoubleSuccess.params).toBe(input.adminSessionId);//consultamos al doble como fue llamado
       expect(output.adminSession).toBe(databaseFindAdminSessionDoubleSuccess.result);
       done();
     });
